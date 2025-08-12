2e5afb162587545ea425c07f3f9b47c27737462f
Amund <amund.ersland@bouvet.no>
2025-08-12 09:20:10 +0200

    das commit

--- main.ts
+++ main.ts
@@ -1,134 +1,118 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, Editor, MarkdownView, Setting, PluginSettingTab } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface RegexReplacement {
+    search: string;
+    replace: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+export default class PrettyLinkTitlesPlugin extends Plugin {
+    public settings: RegexReplacement[] = [];
+
+    async onload() {
+        // Load settings
+        await this.loadSettings();
+
+        // Add command
+        this.addCommand({
+            id: "prettify-links",
+            name: "Prettify Internal Link Titles",
+            editorCallback: (editor: Editor, view: MarkdownView) => {
+                const lineCount = editor.lineCount();
+
+                for (let lineNum = 0; lineNum < lineCount; lineNum++) {
+                    const line = editor.getLine(lineNum);
+
+                    // Only process lines that contain potential links
+                    if (line.includes("[[")) {
+                        const prettifiedLine = this.prettifyLinksInLine(line);
+
+                        // Replace line only if changed
+                        if (prettifiedLine !== line) {
+                            editor.setLine(lineNum, prettifiedLine);
+                        }
+                    }
+                }
+            },
+        });
+
+        // Add settings tab
+        this.addSettingTab(new PrettyLinkTitlesSettings(this.app, this));
+    }
+
+    async loadSettings() {
+        this.settings = await this.loadData() || [];
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
+
+    prettifyLinksInLine(line: string): string {
+        const regex = /\[\[([^\|\]]+)(?:\|([^\]]+))?\]\]/g;
+
+        return line.replace(regex, (match, link, alias) => {
+            if (alias) return match; // Already has a title
+
+            let pretty = link;
+
+            // Apply all regex replacements in order
+            for (const replacement of this.settings) {
+                try {
+                    const searchRegex = new RegExp(replacement.search, 'g');
+                    pretty = pretty.replace(searchRegex, replacement.replace);
+                } catch (e) {
+                    console.error("Invalid regex pattern:", replacement.search, e);
+                }
+            }
+
+            return `[[${link}|${pretty}]]`;
+        });
+    }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class PrettyLinkTitlesSettings extends PluginSettingTab {
+    constructor(app: App, private plugin: PrettyLinkTitlesPlugin) {
+        super(app, plugin);
+    }
 
-	async onload() {
-		await this.loadSettings();
+    display(): void {
+        const { containerEl } = this;
+        containerEl.empty(); // Clear any previous content
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        containerEl.createEl('h2', { text: 'Pretty Link Titles Settings' });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        // Add a button to add new regex replacement
+        new Setting(containerEl)
+            .setName('Add Regex Replacement')
+            .setDesc('Add a new regex search/replace pair')
+            .addButton((button) => {
+                button.setButtonText('Add')
+                    .onClick(async () => {
+                        this.plugin.settings.push({ search: '', replace: '' });
+                        await this.plugin.saveSettings();
+                        this.display(); // Refresh the settings tab
+                    });
+            });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        // Display existing settings
+        for (const [index, setting] of this.plugin.settings.entries()) {
+            new Setting(containerEl)
+                .setName(`Search ${index + 1}`)
+                .addText(text => text
+                    .setValue(setting.search)
+                    .onChange(async (value) => {
+                        this.plugin.settings[index].search = value;
+                        await this.plugin.saveSettings();
+                    }));
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+            new Setting(containerEl)
+                .setName(`Replace ${index + 1}`)
+                .addText(text => text
+                    .setValue(setting.replace)
+                    .onChange(async (value) => {
+                        this.plugin.settings[index].replace = value;
+                        await this.plugin.saveSettings();
+                    }));
+        }
+    }
 }
